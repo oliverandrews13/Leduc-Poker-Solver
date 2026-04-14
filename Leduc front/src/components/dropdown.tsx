2fc873f0c15b5c9d51df353efa88cb3b2f03a9f8
drewnicorn15 <drewdietrich1@gmail.com>
2026-04-14 01:54:27 -0400

    add reacted frontend + some work on it

---
+++ Leduc front/src/components/dropdown.tsx
@@ -0,0 +1,22 @@
+"use client"
+const Dropdown = ({playerNum}) => {
+
+    const changeCard = async (card:string) =>
+    {
+        await fetch("/SetCard",
+        {
+            method:"POST",
+            body: JSON.stringify({player:playerNum,cardID:card})
+        }
+        );
+    }
+    return (
+        <select onChange={(e) => changeCard(e.target.value)} className="rounded-lg bg-red-200 text-black">
+            <option value="J">J</option>
+            <option value="Q">Q</option>
+            <option value="K">K</option>
+        </select>
+    )
+}
+
+export default Dropdown;
